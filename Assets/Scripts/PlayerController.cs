cc8fabc70efd5fed832f33d566e3c2061700c64f
Atte Lindroth <av.lindroth@gmail.com>
2025-11-25 18:01:22 +0200

    PlayerController
    I made script for controlling player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * movementSpeed;
+        _rb.velocity = movement;
+    }
+}

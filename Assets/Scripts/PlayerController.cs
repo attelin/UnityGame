3b8a2b66446f25dfcdd0e1eb58f2c3d9de1f0d69
Atte Lindroth <av.lindroth@gmail.com>
2025-11-26 19:06:33 +0200

    Added animations
    Made few animations for player idle movement and death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _characterBody;
+    [SerializeField] private float _movementSpeed;   
     private Rigidbody2D _rb;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        HandlePlayerMovement();
+    }
+
+
+    private void HandlePlayerMovement()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * movementSpeed;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * _movementSpeed;
         _rb.velocity = movement;
+
+        bool characterIsWalking = movement.magnitude > 0f;
+        _animator.SetBool("isWalking", characterIsWalking);
+
+        bool flipSprite = movement.x < 0f;
+        _characterBody.flipX = flipSprite;
     }
 }

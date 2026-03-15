a2911696ca1425dc9dd214418dfe5d59f167c48a
Atte Lindroth <av.lindroth@gmail.com>
2026-03-15 16:55:11 +0200

    updates to projectile code
    small updates

--- Assets/Scripts/PlayerStaffController.cs
+++ Assets/Scripts/PlayerStaffController.cs
@@ -6,33 +6,75 @@ public class PlayerStaffController : MonoBehaviour
 {
     [SerializeField] private Projectile _projectile;
     [SerializeField] private Transform _tip;
-    [SerializeField] private float _fireRate;
-    private float _nextFireTime;
 
+    [SerializeField] private float _fireRate = 5f;
+    [SerializeField] private float _secondaryFireRate = 1.5f;
+
+    [SerializeField] private float _spreadAngle = 15f;
+
+    private float _nextFireTime;
+    private float _nextSecondaryFireTime;
+
+    private Vector2 _mousePosition;
+    private Vector2 _lookDirection;
 
     void Update()
     {
+        SetMousePosition();
+        SetLookDirection();
         RotateStaff();
+
+        // Primary attack (vasen hiiri)
         if (Input.GetButton("Fire1") && Time.time >= _nextFireTime)
         {
             _nextFireTime = Time.time + 1f / _fireRate;
             Shoot();
         }
-    }
 
+        // Secondary attack (oikea hiiri)
+        if (Input.GetButtonDown("Fire2") && Time.time >= _nextSecondaryFireTime)
+        {
+            _nextSecondaryFireTime = Time.time + 1f / _secondaryFireRate;
+            ShotgunAttack();
+        }
+    }
 
     private void RotateStaff()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDirection = (mousePosition - (Vector2)transform.position).normalized;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
-
     private void Shoot()
     {
-        Debug.Log("BANG!");
+        Projectile newProjectile = Instantiate(_projectile, _tip.position, _tip.rotation);
+        newProjectile.InitializeProjectile(_lookDirection);
     }
 
+    private void ShotgunAttack()
+    {
+        FireProjectile(_lookDirection);
+
+        Vector2 leftDirection = Quaternion.Euler(0, 0, -_spreadAngle) * _lookDirection;
+        FireProjectile(leftDirection);
+
+        Vector2 rightDirection = Quaternion.Euler(0, 0, _spreadAngle) * _lookDirection;
+        FireProjectile(rightDirection);
+    }
+
+    private void FireProjectile(Vector2 direction)
+    {
+        Projectile newProjectile = Instantiate(_projectile, _tip.position, Quaternion.identity);
+        newProjectile.InitializeProjectile(direction);
+    }
+
+    private void SetMousePosition()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void SetLookDirection()
+    {
+        _lookDirection = (_mousePosition - (Vector2)transform.position).normalized;
+    }
 }
